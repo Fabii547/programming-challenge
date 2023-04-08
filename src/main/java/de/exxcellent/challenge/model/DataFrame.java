7a40d612fbe829499c5c7d80c739d15abcf28297
Fabii547 <Fabii547@users.noreply.github.com>
2023-04-08 11:42:58 +0200

    adds a `DataFrame` model for storing tabular data

---
+++ src/main/java/de/exxcellent/challenge/model/DataFrame.java
@@ -0,0 +1,73 @@
+package de.exxcellent.challenge.model;
+
+import java.util.*;
+
+/**
+ * Implementation of an immutable data store for tabular data.
+ */
+public class DataFrame {
+    private final List<String> columns;
+    private final List<Row> rows = new ArrayList<>();
+
+    public DataFrame(String[] columns) throws ColumnHeadersFormatException {
+        String[] sanitizedColumns = this.sanitize(columns);
+        if (sanitizedColumns.length != columns.length) {
+            throw new ColumnHeadersFormatException();
+        }
+
+        this.columns = new ArrayList<>(Arrays.asList(sanitizedColumns));
+    }
+
+    public void addRow(String[] values) throws ColumnMismatchException {
+        if (values.length != this.columns.size()) {
+            throw new ColumnMismatchException();
+        }
+        this.rows.add(new Row(values));
+    }
+
+    public List<Row> getRows() {
+        return this.rows;
+    }
+
+    private String[] sanitize(String[] columns) {
+        return Arrays.stream(columns)
+                .filter(s -> s.length() != 0)
+                .map(String::trim)
+                .map(String::toLowerCase)
+                .toArray(String[]::new);
+    }
+
+    private int getColumnIndexByName(String columnName) {
+        return this.columns.indexOf(columnName);
+    }
+
+    public class Row {
+        private final String[] values;
+
+        public Row(String[] values) {
+            this.values = values;
+        }
+
+        public String getColumnValue(String columnName) {
+            return this.values[getColumnIndexByName(columnName)];
+        }
+    }
+
+    public static class DataFrameException extends Exception {
+        public DataFrameException(String message) {
+            super(message);
+        }
+    }
+
+    public static class ColumnHeadersFormatException extends DataFrameException {
+        public ColumnHeadersFormatException() {
+            super("The passed columns contain either empty strings or duplicate values.");
+        }
+    }
+
+    public static class ColumnMismatchException extends DataFrameException {
+        public ColumnMismatchException() {
+            super("The number of column names does not match the number of passed row values.");
+        }
+    }
+}
---
+++ src/test/java/de/exxcellent/challenge/DataFrameTest.java
@@ -0,0 +1,43 @@
+package de.exxcellent.challenge;
+
+import de.exxcellent.challenge.model.DataFrame;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class DataFrameTest {
+
+    @Test
+    public void testSize() throws DataFrame.ColumnHeadersFormatException {
+        DataFrame df = new DataFrame(new String[]{"First", "Second", "Third"});
+        assertEquals(0, df.getRows().size());
+    }
+
+    @Test
+    public void testEmptyHeader() {
+        assertThrows(
+                DataFrame.ColumnHeadersFormatException.class,
+                () -> new DataFrame(new String[]{"First", "", "Third"})
+        );
+    }
+
+    @Test
+    public void testMismatchingColumns() {
+        assertThrows(DataFrame.ColumnMismatchException.class, () -> {
+            DataFrame df = new DataFrame(new String[]{"First", "Second", "Third"});
+            df.addRow(new String[]{"1", "2", "3", "4"});
+        });
+    }
+
+    @Test
+    public void testGettingRowValue() throws DataFrame.DataFrameException {
+        DataFrame df = new DataFrame(new String[]{"First", "Second", "Third"});
+        df.addRow(new String[]{"1", "2", "3"});
+
+        List<DataFrame.Row> rows = df.getRows();
+        assertEquals("2", rows.get(0).getColumnValue("second"));
+    }
+}
