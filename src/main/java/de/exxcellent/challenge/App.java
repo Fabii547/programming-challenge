a182e6d88345c34ddcf3ac5d8480010f7059f019
Fabii547 <Fabii547@users.noreply.github.com>
2023-04-10 09:25:34 +0200

    chore(cleanup): clean up

--- src/main/java/de/exxcellent/challenge/App.java
+++ src/main/java/de/exxcellent/challenge/App.java
@@ -7,19 +7,11 @@ import de.exxcellent.challenge.reader.CSVDataFrameReader;
 
 
 /**
- * The entry class for your solution. This class is only aimed as starting point and not intended as baseline for your software
- * design. Read: create your own classes and packages as appropriate.
- *
- * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ * The entry class of the program.
  */
 public final class App {
 
-    /**
-     * This is the main entry method of your program.
-     * @param args The CLI arguments passed
-     */
     public static void main(String... args) {
-
         try {
             DataFrame weatherDF = new CSVDataFrameReader().parseIntoDataFrame("src/main/resources/de/exxcellent/challenge/weather.csv");
             new WeatherDataAnalyzer().analyze(weatherDF);
